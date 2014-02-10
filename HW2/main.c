b6a3feb1234c035e710deb21a87b67684392a37e
Matthew Bender <quartarian@gmail.com>
2014-02-10 12:38:39 -0500

    Finished\!

--- HW2/main.c
+++ HW2/main.c
@@ -1,11 +1,31 @@
+// Matthew Bender
+// 02/10/14
+
 #include <stdio.h>
 
 unsigned int octtoint(unsigned int a, unsigned int b, unsigned int c, unsigned int d) {
-    d += (c << 4);
-    d += (b << 8);
-    d += (a << 16);
-    return d;
+    unsigned int output = d;
+    output += (c << 8);
+    output += (b << 16);
+    output += (a << 24);
+    return output;
 }
+void intobin(unsigned int num) {
+    int i = 0;
+    printf("\nBinary: ");
+    unsigned char c = '\0';
+    unsigned int MASK = 1<<31;   
+    
+    for(i = 0; i < 32; i++ ){
+        if( i > 0 && i % 8 == 0 ) printf(" ");
+        if( (num & MASK) == MASK ) printf("1");
+        else printf("0");
+        MASK = MASK >> 1;
+     }
+     printf("\n");
+
+}
+
 
 int main() {
     unsigned int octa1, octa2, octa3, octa4;
@@ -13,26 +33,28 @@ int main() {
     unsigned int octc1, octc2, octc3, octc4;
 
     printf("Please enter the first IP address:");
-    scanf("%i.%i.%i.%i", &octa1, &octa2, &octa3, &octa4);
+    scanf("%d.%d.%d.%d", &octa1, &octa2, &octa3, &octa4);
 
-    octa4 = octa4 << 1;
-    printf(octa4);    
-    
-    
-    /* 
-    printf(octtoint(octa1,octa2,octa3,octa4));
-
-    printf("Please enter the subnet:");
-    scanf("%i.%i.%i.%i", &octb1, &octb2, &octb3, &octb4);
+    printf("Please the subnet:");
+    scanf("%d.%d.%d.%d", &octb1, &octb2, &octb3, &octb4);
 
     printf("Please enter the second IP address:");
-    scanf("%i.%i.%i.%i", &octc1, &octc2, &octc3, &octc4);
-    */
+    scanf("%d.%d.%d.%d", &octc1, &octc2, &octc3, &octc4);
 
 
+    unsigned int ip1, ip2, subnet;
+    ip1 = octtoint(octa1,octa2,octa3,octa4);
+    ip2 = octtoint(octc1,octc2,octc3,octc4);
+    subnet = octtoint(octb1,octb2,octb3,octb4);
 
-    //printf("\nYou entered %i, %i, %i, %i\n",octa1,octa2,octa3,octa4);
 
+    printf("-----------------------------------\n");
+    if((ip1 & subnet) == (ip2 & subnet)) {
+        printf("They are on the same network.\n");
+    }
+    else {
+        printf("They are NOT on the same network.\n");
+    }
 
 
 }
